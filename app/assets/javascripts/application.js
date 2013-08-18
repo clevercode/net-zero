fc9dcf22847d64465ecfa1d94cab923b4f362c88
Zachary Nicoll <tower@zdn.me>
2013-08-17 22:01:21 -0400

    Removed unslider and dependancies

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -3,9 +3,6 @@
 
 //= require jquery
 //= require jquery-ui-1.10.3.custom
-//= require jquery.event.move
-//= require jquery.event.swipe
-//= require unslider
 
 //= require turbolinks
 //= require netZero
