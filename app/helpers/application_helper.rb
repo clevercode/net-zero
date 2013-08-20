bffec7a9c96ba4c766f716214754e79287ec02a8
Zachary Nicoll <tower@zdn.me>
2013-08-19 20:00:21 -0400

    Changed startup images

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,28 +1,7 @@
 module ApplicationHelper
 
-  IOS_DEVICE_WIDTH = {
-    :ipad => 768,
-    :ipad_retina => 1536,
-    :iphone_retina => 640
-  }
-
-  def ios_startup_image_tag(image, device, orientation)
-    tag :link, rel: 'apple-touch-startup-image', 
-              href: path_to_image(image),
-              media: ios_media_query(device, orientation)
-  end
-
   def ios_touch_icon_tag(image, size)
     tag :link, rel: 'apple-touch-icon-precomposed', href: path_to_image(image), sizes: size
-  end
-
-  def ios_media_query(device, orientation)
-    width = '%spx' % IOS_DEVICE_WIDTH[device]
-    query = "(device-width: #{width}) and (orientation: #{orientation})"
-    if device =~ /retina/
-      query.concat "and (-webkit-device-pixel-ratio: 2)"
-    end
-    query
   end
 
 end
